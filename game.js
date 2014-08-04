7f4c40c0f16c9aee4dccb6cea7d49af77a8d15a3
Rather <supertyrian@gmail.com>
2014-08-03 23:03:10 -0700

    movement, updated shader

--- game.js
+++ game.js
@@ -6,6 +6,53 @@ renderer.setSize(window.innerWidth * 0.90, window.innerHeight * 0.90);
 document.body.appendChild(renderer.domElement);
 
 
+
+scene.add(new THREE.GridHelper(20, 1));
+var axisHelper = new THREE.AxisHelper();
+axisHelper.position.y += 0.001;
+scene.add(axisHelper);
+
+var cube = buildCube();
+cube.position.x = 5;
+cube.position.z = -2;
+scene.add(cube);
+
+var light = new THREE.DirectionalLight(0xffffff, 0.5);
+light.position.x = -1;
+light.position.y = 1;
+light.position.z = 1;
+scene.add(light);
+light = new THREE.HemisphereLight(0xffffff, 0x404040, 0.5);
+scene.add(light);
+
+for(var i=0; i<6; i++) {
+  var tree = buildTree();
+  tree.position.x = Math.random() * 10;
+  tree.position.z = Math.random() * 10;
+  scene.add(tree);
+}
+
+var terrain = buildTerrainMesh();
+scene.add(terrain);
+
+var player = buildPlayer();
+scene.add(player);
+
+camera.position.z = -1.5;
+camera.position.x = -1.5;
+camera.position.y = 2.5;
+camera.lookAt(new THREE.Vector3());
+
+
+
+function buildPlayer() {
+  var geometry = new THREE.SphereGeometry(0.25, 32, 32);
+  var material = new THREE.MeshLambertMaterial({ color: 0xFB966E });
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = 0.25;
+  return mesh;
+}
+
 function buildCube() {
   var geometry = new THREE.BoxGeometry(1, 1, 1);
   var material = new THREE.ShaderMaterial({
@@ -14,36 +61,12 @@ function buildCube() {
       time: { type:"f", value: 1.0 },
       resolution: { type: "v2", value: new THREE.Vector2() }
     },
-    vertexShader: vertexShader('basic'),
-    fragmentShader: fragmentShader('basic')
+    vertexShader: vertexShader('terrain'),
+    fragmentShader: fragmentShader('terrain')
   });
   var cube = new THREE.Mesh(geometry, material);
   return cube;
 }
-
-var cube = buildCube();
-cube.position.x = 5;
-cube.position.z = -2;
-scene.add(cube);
-
-var light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.x = 5;
-light.position.y = 2;
-light.position.z = 5;
-scene.add(light);
-
-var terrain = buildTerrainMesh();
-scene.add(terrain);
-
-scene.add(new THREE.GridHelper(20, 1));
-var axisHelper = new THREE.AxisHelper();
-axisHelper.position.y += 0.001;
-scene.add(axisHelper);
-
-camera.position.z = -2.5;
-camera.position.x = -2.5;
-camera.position.y = 2.5;
-camera.lookAt(new THREE.Vector3());
 
 function buildTerrainMesh() {
   var geometry = new THREE.Geometry();
@@ -88,8 +111,8 @@ function buildTerrainMesh() {
       time: { type:"f", value: 1.0 },
       resolution: { type: "v2", value: new THREE.Vector2() }
     },
-    vertexShader: vertexShader('basic'),
-    fragmentShader: fragmentShader('basic')
+    vertexShader: vertexShader('terrain'),
+    fragmentShader: fragmentShader('terrain')
   });
 
   //var material = new THREE.MeshLambertMaterial({ color: 0x00aa00 });
@@ -98,13 +121,43 @@ function buildTerrainMesh() {
   return mesh;
 }
 
+function buildTree() {
+  var geometry = new THREE.BoxGeometry(0.3, 3, 0.3);
+  var material = new THREE.MeshLambertMaterial({ color: 0xECB88A });
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = 1.5;
+  return mesh;
+}
+
 function update(delta) {
   cube.rotation.x += 1 * delta;
   cube.rotation.y += 1 * delta;
+
+  handleInput(delta);
 }
 
 
+var handleInput = (function() {
+  var input = new THREEx.KeyboardState();
+  var playerMoveSpeed = 2.0;
 
+  return function(delta) {
+    if(input.pressed('up')) {
+      player.position.x += playerMoveSpeed * delta;
+    }
+    if(input.pressed('down')) {
+      player.position.x -= playerMoveSpeed * delta;
+    }
+    if(input.pressed('right')) {
+      player.position.z += playerMoveSpeed * delta;
+    }
+    if(input.pressed('left')) {
+      player.position.z -= playerMoveSpeed * delta;
+    }
+    camera.position.x = player.position.x - 1.5;
+    camera.position.z = player.position.z - 1.5;
+  }
+})();
 
 var clock = new THREE.Clock(true);
 
